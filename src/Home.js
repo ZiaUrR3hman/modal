bcd195a7d8230d9d8a5dfbe37906bedac45747ac
Zia Ur Rehman <ziaurr3hman@hotmail.com>
2021-03-14 22:20:51 +0500

    useContext

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 const Home = () => {
-	const [openModal, setOpenModal] = useState(false);
 	return (
 		<>
 			<div className="container mx-auto bg-white shadow-lg">
@@ -12,7 +11,7 @@ const Home = () => {
 						}}
 						className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-offset-2 px-4 py-2 text-white font-semibold text-lg tracking-wider uppercase rounded-sm"
 					>
-						Open modal
+						Deactivate
 					</button>
 				</div>
 			</div>
