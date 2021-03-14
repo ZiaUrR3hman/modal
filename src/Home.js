e9eb1b0fe5bae507314231f34503ef503ab48093
Zia Ur Rehman <ziaurr3hman@hotmail.com>
2021-03-14 22:42:04 +0500

    react modal

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import { useGlobalContext } from './context';
 
 const Home = () => {
+	const { openModal } = useGlobalContext();
 	return (
 		<>
 			<div className="container mx-auto bg-white shadow-lg">
 				<div className="flex items-center justify-center h-screen ">
 					<button
-						onClick={() => {
-							setOpenModal(true);
-						}}
+						onClick={openModal}
 						className="bg-indigo-500 hover:bg-indigo-600 active:bg-indigo-700 focus:outline-none focus:ring focus:ring-offset-2 px-4 py-2 text-white font-semibold text-lg tracking-wider uppercase rounded-sm"
 					>
 						Deactivate
